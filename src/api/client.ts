bb9a4eff8048b5e22529931dba0d7fc2395f2811
hugme4944 <hugme4944@gmail.com>
2020-12-23 04:26:40 +0900

    Clear localStorage and reload on error 'Unauthorized'

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,15 @@
 import { ApolloClient, ApolloLink, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/link-error';
 import fetch from 'cross-fetch';
 import { inMemoryCache } from './cache';
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors?.some((error) => error.message === 'Unauthorized')) {
+    localStorage.clear();
+    window.location.reload();
+  }
+});
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
@@ -19,6 +27,6 @@ const httpLink = createHttpLink({
 });
 
 export const client = new ApolloClient({
-  link: ApolloLink.from([authLink, httpLink]),
+  link: ApolloLink.from([errorLink, authLink, httpLink]),
   cache: inMemoryCache,
 });
