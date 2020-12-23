d13949e9dea61ec9e71844e7f075fbcd08f48fb3
hugme4944 <hugme4944@gmail.com>
2020-12-23 21:16:01 +0900

    feat: Add 'to' prop to ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,11 +1,25 @@
 import React, { ButtonHTMLAttributes, FC } from 'react';
+import { useHistory } from 'react-router-dom';
 
-interface ListItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ListItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  to?: string;
+}
 
-export const ListItem: FC<ListItemProps> = ({ children, ...props }) => {
+export const ListItem: FC<ListItemProps> = ({
+  children,
+  to,
+  onClick,
+  ...props
+}) => {
+  const history = useHistory();
+
   return (
     <li>
-      <button {...props} className="px-5 w-full">
+      <button
+        {...props}
+        className="px-5 w-full"
+        onClick={to ? () => history.push(to) : onClick}
+      >
         <div className="py-4 border-b border-gray-100">{children}</div>
       </button>
     </li>
