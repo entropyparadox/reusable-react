0769ae9815462ecc6c1a14ba1d7464defde766cb
jihunsuh <jihunsuh12@gmail.com>
2020-12-28 14:39:34 +0900

    Section spaceY customable change

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,16 +3,18 @@ import React, { FC } from 'react';
 interface SectionProps {
   flexDirection?: 'flex-col' | 'flex-row';
   alignItems?: 'items-stretch' | 'items-center';
+  spaceY?: 'space-y-2' | 'space-y-4';
 }
 
 export const Section: FC<SectionProps> = ({
   children,
   flexDirection = 'flex-col',
   alignItems = 'items-stretch',
+  spaceY = 'space-y-4',
 }) => {
   return (
     <section
-      className={`flex ${flexDirection} ${alignItems} space-y-4 px-5 py-4 bg-white`}
+      className={`flex ${flexDirection} ${alignItems} ${spaceY} px-5 py-4 bg-white`}
     >
       {children}
     </section>
