56b887bdfe5b4a33cd1f9cb003bf2b595872ce10
jihunsuh <jihunsuh12@gmail.com>
2021-01-04 18:41:21 +0900

    add Blank

---
+++ src/components/Blank.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { use100vh } from 'react-div-100vh';
+
+interface BlankProps {
+  text?: string;
+}
+
+export const Blank: FC<BlankProps> = ({ text, children }) => {
+  let vh = use100vh();
+  const height = vh ? `${vh}px` : '100vh';
+
+  if (!text && !children) {
+    text = 'LOADING...';
+  }
+
+  return (
+    <div
+      className="fixed inset-0 w-full h-screen z-30 bg-black opacity-50 flex items-center justify-center"
+      style={{ height }}
+      onClick={e => {
+        e.preventDefault();
+        e.stopPropagation();
+      }}
+    >
+      <div className="text-white text-2xl">
+        {text} {children}
+      </div>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './AspectRatioImage';
 export * from './AuthRoute';
+export * from './Blank';
 export * from './BottomFixedButton';
 export * from './BottomFixedView';
 export * from './BottomNavbar';
