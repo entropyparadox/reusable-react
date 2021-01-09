95b4a0897484649389dddf2578e462f5688c37db
hugme4944 <hugme4944@gmail.com>
2021-01-09 17:54:43 +0900

    add disabled styles to TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -21,7 +21,8 @@ export const TextField: FC<TextFieldProps> = ({
       type={type}
       className="block px-4 w-full h-12
                  border-gray-200 rounded-md sm:text-sm
-                 focus:ring-0 focus:border-brand-1 placeholder-gray-400"
+                 focus:ring-0 focus:border-brand-1 placeholder-gray-400
+                 disabled:bg-gray-100 disabled:text-gray-400"
     />
   );
 
