e6e09116f411909f09595ad9f686749fd52ffc81
hugme4944 <hugme4944@gmail.com>
2021-01-10 22:59:37 +0900

    add fullBorder prop to ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,24 +3,28 @@ import { useHistory } from 'react-router-dom';
 
 interface ListItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   to?: string;
+  fullBorder?: boolean;
 }
 
 export const ListItem: FC<ListItemProps> = ({
   children,
   to,
+  fullBorder = false,
   onClick,
   ...props
 }) => {
   const history = useHistory();
+  const borderOuter = fullBorder ? 'border-b' : 'border-0';
+  const borderInner = fullBorder ? 'border-0' : 'border-b';
 
   return (
-    <li>
+    <li className={`bg-white ${borderOuter}`}>
       <button
         {...props}
         className="px-5 w-full"
         onClick={to ? () => history.push(to) : onClick}
       >
-        <div className="py-4 border-b border-gray-100">{children}</div>
+        <div className={`py-4 ${borderInner}`}>{children}</div>
       </button>
     </li>
   );
