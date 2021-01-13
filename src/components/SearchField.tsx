120f89740c3e3728536652854491174de783f107
hugme4944 <hugme4944@gmail.com>
2021-01-13 18:31:33 +0900

    add SearchField component

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,16 +1,48 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, { FC, InputHTMLAttributes, useRef } from 'react';
+import { ReactComponent as SearchCloseIcon } from '../assets/images/icon-search-close.svg';
 import { ReactComponent as SearchIcon } from '../assets/images/icon-search.svg';
 
-interface SearchFieldProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface SearchFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  onSearch?: () => void;
+}
 
-export const SearchField: FC<SearchFieldProps> = ({ ...props }) => {
+export const SearchField: FC<SearchFieldProps> = ({
+  onSearch = () => {},
+  value,
+  onChange,
+  ...props
+}) => {
+  const input = useRef<HTMLInputElement>(null);
+
   return (
-    <div className="flex items-center border border-gray-200 focus-within:border-brand-1 rounded-lg px-4 py-2 ps:py-3">
-      <SearchIcon />
+    <form
+      className="flex items-center space-x-2 px-4 w-full h-10
+                 rounded-full border border-gray-200 focus-within:border-brand-1"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch();
+      }}
+    >
       <input
-        className="block px-4 w-full text-sm ps:text-base focus:ring-0 placeholder-gray-400"
         {...props}
+        ref={input}
+        className="w-full text-sm focus:ring-0 placeholder-gray-400"
+        value={value}
+        onChange={onChange}
       />
-    </div>
+      {value && (
+        <SearchCloseIcon
+          onClick={() => {
+            Object.getOwnPropertyDescriptor(
+              window.HTMLInputElement.prototype,
+              'value',
+            )?.set?.call(input.current, '');
+            input.current?.dispatchEvent(
+              new Event('change', { bubbles: true }),
+            );
+          }}
+        />
+      )}
+    </form>
   );
 };
