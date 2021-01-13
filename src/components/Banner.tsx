79d508f28e8642faf63926b38a680c17c44ef660
hugme4944 <hugme4944@gmail.com>
2021-01-14 02:17:31 +0900

    add to prop to Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,20 @@
-import React, { FC } from 'react';
+import React, { ButtonHTMLAttributes, FC } from 'react';
+import { useHistory } from 'react-router-dom';
 
-interface BannerProps {
+interface BannerProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   src?: string;
+  to?: string;
   height?: 'h-18' | 'h-24' | 'h-32';
 }
 
-export const Banner: FC<BannerProps> = ({ src, height = 'h-24' }) => {
+export const Banner: FC<BannerProps> = ({
+  src,
+  to,
+  height = 'h-24',
+  onClick,
+  ...props
+}) => {
+  const history = useHistory();
   let style: Record<string, string> = { width: 'calc(100vw - 2.5rem)' };
 
   if (src) {
@@ -16,9 +25,13 @@ export const Banner: FC<BannerProps> = ({ src, height = 'h-24' }) => {
   }
 
   return (
-    <li
-      style={style}
-      className={`rounded-xl bg-center bg-no-repeat bg-cover bg-gray-500 ${height}`}
-    ></li>
+    <li>
+      <button
+        {...props}
+        style={style}
+        className={`rounded-xl bg-center bg-no-repeat bg-cover bg-gray-500 ${height}`}
+        onClick={to ? () => history.push(to) : onClick}
+      ></button>
+    </li>
   );
 };
