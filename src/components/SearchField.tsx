78148faf70cd31ec70df7dc0090e1b2077c1e960
hugme4944 <hugme4944@gmail.com>
2021-01-13 18:50:11 +0900

    fix unexpected glitch on SearchField

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,6 +1,7 @@
 import React, { FC, InputHTMLAttributes, useRef } from 'react';
 import { ReactComponent as SearchCloseIcon } from '../assets/images/icon-search-close.svg';
 import { ReactComponent as SearchIcon } from '../assets/images/icon-search.svg';
+import { IconButton } from './IconButton';
 
 interface SearchFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   onSearch?: () => void;
@@ -16,7 +17,7 @@ export const SearchField: FC<SearchFieldProps> = ({
 
   return (
     <form
-      className="flex items-center space-x-2 px-4 w-full h-10
+      className="flex items-center px-4 w-full h-10
                  rounded-full border border-gray-200 focus-within:border-brand-1"
       onSubmit={(e) => {
         e.preventDefault();
@@ -26,22 +27,25 @@ export const SearchField: FC<SearchFieldProps> = ({
       <input
         {...props}
         ref={input}
-        className="w-full text-sm focus:ring-0 placeholder-gray-400"
+        className="flex-1 text-sm focus:ring-0 placeholder-gray-400"
         value={value}
         onChange={onChange}
       />
       {value && (
-        <SearchCloseIcon
-          onClick={() => {
-            Object.getOwnPropertyDescriptor(
-              window.HTMLInputElement.prototype,
-              'value',
-            )?.set?.call(input.current, '');
-            input.current?.dispatchEvent(
-              new Event('change', { bubbles: true }),
-            );
-          }}
-        />
+        <div className="-mr-2">
+          <IconButton
+            icon={SearchCloseIcon}
+            onClick={() => {
+              Object.getOwnPropertyDescriptor(
+                window.HTMLInputElement.prototype,
+                'value',
+              )?.set?.call(input.current, '');
+              input.current?.dispatchEvent(
+                new Event('change', { bubbles: true }),
+              );
+            }}
+          />
+        </div>
       )}
     </form>
   );
