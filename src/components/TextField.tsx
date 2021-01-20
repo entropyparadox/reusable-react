875ad2aba2dca9000d4dabbbf912f446bcd653ae
hugme4944 <hugme4944@gmail.com>
2021-01-21 07:23:19 +0900

    remove explicit type prop from TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -3,22 +3,16 @@ import { useId } from 'react-id-generator';
 import { Label } from './Label';
 
 interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  type?: 'text' | 'email' | 'password' | 'tel';
   label?: string;
 }
 
-export const TextField: FC<TextFieldProps> = ({
-  type = 'text',
-  label,
-  ...props
-}) => {
+export const TextField: FC<TextFieldProps> = ({ label, ...props }) => {
   const [htmlId] = useId(1, 'textfield');
 
   const input = (
     <input
       {...props}
       id={htmlId}
-      type={type}
       className="block px-4 w-full h-12
                  border-gray-200 rounded-md sm:text-sm
                  focus:ring-0 focus:border-brand-1 placeholder-gray-400
