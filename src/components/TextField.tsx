8a17d31af1d4e23c21ac7c0ee2382c489f4942f7
hugme4944 <hugme4944@gmail.com>
2021-01-21 11:18:13 +0900

    add default type text for TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -3,16 +3,22 @@ import { useId } from 'react-id-generator';
 import { Label } from './Label';
 
 interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  type?: string;
   label?: string;
 }
 
-export const TextField: FC<TextFieldProps> = ({ label, ...props }) => {
+export const TextField: FC<TextFieldProps> = ({
+  type = 'text',
+  label,
+  ...props
+}) => {
   const [htmlId] = useId(1, 'textfield');
 
   const input = (
     <input
       {...props}
       id={htmlId}
+      type={type}
       className="block px-4 w-full h-12
                  border-gray-200 rounded-md sm:text-sm
                  focus:ring-0 focus:border-brand-1 placeholder-gray-400
