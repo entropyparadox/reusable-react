898b55268e8f6dc8599e62f974aaacb441575aae
hugme4944 <hugme4944@gmail.com>
2021-01-22 16:51:14 +0900

    add size to minWidth and minHeight

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,8 @@ export const reusableConfig = (config: any) => {
   if (config.theme.fontFamily.sans === undefined)
     config.theme.fontFamily.sans = [];
   if (config.theme.fontSize === undefined) config.theme.fontSize = {};
+  if (config.theme.minWidth === undefined) config.theme.minWidth = {};
+  if (config.theme.minHeight === undefined) config.theme.minHeight = {};
   if (config.theme.truncate === undefined) config.theme.truncate = {};
   if (config.theme.truncate.lines === undefined)
     config.theme.truncate.lines = {};
@@ -17,21 +19,44 @@ export const reusableConfig = (config: any) => {
     config.variants.borderColor = [];
   }
 
-  config.plugins = [
-    ...config.plugins,
-    require('@tailwindcss/forms'),
-    require('tailwindcss-truncate-multiline')(),
-  ];
-
-  config.theme.colors = {
-    ...config.theme.colors,
-    'kakao-1': '#f6e24b',
-    'kakao-2': '#46272a',
-  };
-
-  config.theme.spacing = {
-    ...config.theme.spacing,
+  const size = {
+    px: '1px',
+    0: '0px',
+    0.5: '0.125rem',
+    1: '0.25rem',
+    1.5: '0.375rem',
+    2: '0.5rem',
+    2.5: '0.625rem',
+    3: '0.75rem',
+    3.5: '0.875rem',
+    4: '1rem',
+    5: '1.25rem',
+    6: '1.5rem',
+    7: '1.75rem',
+    8: '2rem',
+    9: '2.25rem',
+    10: '2.5rem',
+    11: '2.75rem',
+    12: '3rem',
     13: '3.25rem',
+    14: '3.5rem',
+    16: '4rem',
+    18: '4.5rem',
+    20: '5rem',
+    24: '6rem',
+    28: '7rem',
+    32: '8rem',
+    36: '9rem',
+    40: '10rem',
+    44: '11rem',
+    48: '12rem',
+    52: '13rem',
+    56: '14rem',
+    60: '15rem',
+    64: '16rem',
+    72: '18rem',
+    80: '20rem',
+    96: '24rem',
     '1/2': '50%',
     '1/3': '33.333333%',
     '2/3': '66.666667%',
@@ -60,6 +85,23 @@ export const reusableConfig = (config: any) => {
     '10/12': '83.333333%',
     '11/12': '91.666667%',
     full: '100%',
+  };
+
+  config.plugins = [
+    ...config.plugins,
+    require('@tailwindcss/forms'),
+    require('tailwindcss-truncate-multiline')(),
+  ];
+
+  config.theme.colors = {
+    ...config.theme.colors,
+    'kakao-1': '#f6e24b',
+    'kakao-2': '#46272a',
+  };
+
+  config.theme.spacing = {
+    ...config.theme.spacing,
+    ...size,
     'fixed-top': '2.75rem',
     'fixed-bottom': '3rem',
   };
@@ -78,6 +120,16 @@ export const reusableConfig = (config: any) => {
     '2lg': ['1.1875rem', { lineHeight: '1.875rem' }],
   };
 
+  config.theme.minWidth = {
+    ...config.theme.minWidth,
+    ...size,
+  };
+
+  config.theme.minHeight = {
+    ...config.theme.minHeight,
+    ...size,
+  };
+
   config.theme.truncate.lines = {
     ...config.theme.truncate.lines,
     2: '2',
@@ -88,6 +140,8 @@ export const reusableConfig = (config: any) => {
 
   config.theme.zIndex = {
     auto: 'auto',
+    '-30': '-30',
+    '-20': '-20',
     '-10': '-10',
     0: '0',
     10: '10',
