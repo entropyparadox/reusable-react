897d40205ba6fe6c79ecb2639347152381772eb5
hugme4944 <hugme4944@gmail.com>
2021-01-30 20:17:25 +0900

    add forward ref to Screen

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,33 +1,39 @@
-import React, { FC, HTMLAttributes } from 'react';
+import React, { FC, forwardRef, HTMLAttributes } from 'react';
 import { use100vh } from 'react-div-100vh';
 
 interface ScreenProps extends HTMLAttributes<HTMLDivElement> {
+  position?: 'static' | 'fixed';
   top?: boolean;
   bottom?: boolean;
 }
 
-export const Screen: FC<ScreenProps> = ({
-  children,
-  top = false,
-  bottom = false,
-  ...props
-}) => {
-  let vh = use100vh();
-  const screenHeight = vh ? `${vh}px` : '100vh';
-  let height = screenHeight;
-  if (top) {
-    height = `calc(${screenHeight} - 2.75rem)`;
-  }
-  if (bottom) {
-    height = `calc(${screenHeight} - 3rem)`;
-  }
-  if (top && bottom) {
-    height = `calc(${screenHeight} - 5.75rem)`;
-  }
+export const Screen = forwardRef<HTMLDivElement, ScreenProps>(
+  (
+    { children, position = 'static', top = false, bottom = false, ...props },
+    ref,
+  ) => {
+    let vh = use100vh();
+    const screenHeight = vh ? `${vh}px` : '100vh';
+    let height = screenHeight;
+    if (top) {
+      height = `calc(${screenHeight} - 2.75rem)`;
+    }
+    if (bottom) {
+      height = `calc(${screenHeight} - 3rem)`;
+    }
+    if (top && bottom) {
+      height = `calc(${screenHeight} - 5.75rem)`;
+    }
 
-  return (
-    <div {...props} className="flex flex-col" style={{ height }}>
-      {children}
-    </div>
-  );
-};
+    return (
+      <div
+        {...props}
+        ref={ref}
+        className={`${position} flex flex-col w-full`}
+        style={{ height }}
+      >
+        {children}
+      </div>
+    );
+  },
+);
--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -4,24 +4,31 @@ interface TopNavbarProps {
   title?: string;
   left?: ReactElement;
   right?: ReactElement;
+  position?: 'sticky' | 'fixed';
+  borderless?: boolean;
+  backgroundColor?: 'transparent' | 'white';
   leftFlex?: 'flex-1' | 'flex-none';
   rightFlex?: 'flex-1' | 'flex-none';
-  borderless?: boolean;
 }
 
 export const TopNavbar: FC<TopNavbarProps> = ({
   title,
   left,
   right,
+  position = 'sticky',
   borderless = false,
+  backgroundColor = 'white',
   leftFlex = 'flex-1',
   rightFlex = 'flex-1',
 }) => {
+  const positionAdditional =
+    position === 'sticky' ? 'top-0' : 'top-0 left-0 right-0';
   const border = borderless ? 'border-0' : 'border-b';
 
   return (
     <nav
-      className={`sticky top-0 z-50 flex items-center px-2 h-fixed-top ${border} bg-white`}
+      className={`${position} ${positionAdditional} z-50 flex items-center
+                  px-2 h-fixed-top ${border} bg-${backgroundColor}`}
     >
       <div className={`${leftFlex} flex items-center`}>{left}</div>
       <h1 className="font-bold text-gray-800">{title}</h1>
