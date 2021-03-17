556a33af965cf0a696d79e5c61b6bb1c355a9f77
hugme4944 <hugme4944@gmail.com>
2021-03-17 22:35:02 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -121,6 +121,32 @@ export const reusableConfig = (config: any) => {
     '2lg': ['1.1875rem', { lineHeight: '1.875rem' }],
   };
 
+  config.theme.maxWidth = (theme: any, { breakpoints }: any) => ({
+    none: 'none',
+    xs: '20rem',
+    sm: '24rem',
+    md: '28rem',
+    lg: '32rem',
+    xl: '36rem',
+    '2xl': '42rem',
+    '3xl': '48rem',
+    '4xl': '56rem',
+    '5xl': '64rem',
+    '6xl': '72rem',
+    '7xl': '80rem',
+    min: 'min-content',
+    max: 'max-content',
+    prose: '65ch',
+    ...breakpoints(theme('screens')),
+    ...size,
+  });
+
+  config.theme.maxHeight = (theme: any) => ({
+    ...theme('spacing'),
+    screen: '100vh',
+    ...size,
+  });
+
   config.theme.minWidth = {
     ...config.theme.minWidth,
     ...size,
