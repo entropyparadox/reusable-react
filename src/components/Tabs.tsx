6eee965cd9be74affad61ec483432aebe80e3002
jihunsuh <jihunsuh12@gmail.com>
2021-04-14 16:13:30 +0900

    tabs full

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,14 +1,16 @@
-import React, { FC } from 'react';
+import React, { FC, HTMLAttributes } from 'react';
 
-interface TabsProps {
+interface TabsProps extends HTMLAttributes<HTMLElement> {
   sticky?: boolean;
   padding?: 'p-0' | 'px-3';
+  full: boolean;
   height?: 'h-10' | 'h-16' | 'h-20';
   borderless?: boolean;
 }
 
 export const Tabs: FC<TabsProps> = ({
   children,
+  full = false,
   sticky = false,
   padding = 'p-0',
   height = 'h-10',
@@ -16,10 +18,11 @@ export const Tabs: FC<TabsProps> = ({
 }) => {
   const position = sticky ? 'sticky top-fixed-top z-50' : '';
   const border = borderless ? 'border-0' : 'border-b';
+  const width = full ? 'w-full' : '';
 
   return (
     <nav
-      className={`${position} flex overflow-x-auto ${padding} ${height} ${border} bg-white`}
+      className={`${position} ${width} flex overflow-x-auto ${padding} ${height} ${border} bg-white`}
       style={{ msOverflowStyle: 'none', scrollbarWidth: 'none' }}
     >
       {children}
